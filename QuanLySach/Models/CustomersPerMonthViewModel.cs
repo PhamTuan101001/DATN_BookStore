cc38ee3b6ef2642f7fcb33f559f35db174520128
phamtuan101001 <yukikami101001@gmail.com>
2025-05-16 08:23:47 +0700

    add files

---
+++ QuanLySach/Models/CustomersPerMonthViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySach.Models
+{
+    public class CustomersPerMonthViewModel
+    {
+        public int Month { get; set; }
+        public int CustomerCount { get; set; }
+    }
+
+}
